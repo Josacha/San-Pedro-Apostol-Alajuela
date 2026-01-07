fdaf4ad5f0832c4ea89b73f8616c3528720a9beb
Josacha <120397811+Josacha@users.noreply.github.com>
2026-01-07 16:19:07 -0600

    Update dashboard.js

--- admin/dashboard.js
+++ admin/dashboard.js
@@ -1,9 +1,15 @@
-import { auth } from "../js/firebase-config.js";
-import { onAuthStateChanged } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { auth, db } from "../js/firebase-config.js";
+import { doc, getDoc } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-onAuthStateChanged(auth, user => {
-  if (!user) {
-    window.location.href = "login.html";
+auth.onAuthStateChanged(async user => {
+  const ref = doc(db, "usuarios", user.uid);
+  const snap = await getDoc(ref);
+
+  const rol = snap.data().rol;
+
+  if (rol !== "parroco") {
+    document.getElementById("menu-roles").style.display = "none";
   }
 });
+
